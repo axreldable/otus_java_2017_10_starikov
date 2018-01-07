36307f1e39939e589e24c11f89a99ff0b0f7fd4c
axreldable <starikovaleksei1992@mail.ru>
2018-01-07 11:13:02 +0300

    homework_7 (fix after review) made unchangeable fields final

--- HW07/Department/src/main/java/ru/otus/hw7/department/AtmMementoPair.java
+++ HW07/Department/src/main/java/ru/otus/hw7/department/AtmMementoPair.java
@@ -1,13 +1,12 @@
 package ru.otus.hw7.department;
 
 import lombok.Getter;
-import lombok.Setter;
 import ru.otus.hw7.atm.ATM;
 import ru.otus.hw7.atm.memento.pattern.Memento;
 
 public class AtmMementoPair {
-    @Setter @Getter private Memento memento;
-    @Setter @Getter private ATM atm;
+    @Getter private final Memento memento;
+    @Getter private final ATM atm;
 
     public AtmMementoPair(Memento memento, ATM atm) {
         this.memento = memento;
