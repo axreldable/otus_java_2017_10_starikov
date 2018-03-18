3ab65603a167d3faa628389cb1a8a788da012bd7
axreldable <starikovaleksei1992@mail.ru>
2018-03-18 11:00:46 +0300

    homework_15 (fixes after review) combine frontend and web socket in separate branch

--- HW15/web_server/src/main/java/ru/otus/hw15/front/FrontendServiceImpl.java
+++ HW15/web_server/src/main/java/ru/otus/hw15/front/FrontendServiceImpl.java
@@ -2,6 +2,8 @@ package ru.otus.hw15.front;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.server.standard.SpringConfigurator;
 import ru.otus.hw15.app.FrontendService;
 import ru.otus.hw15.message.MsgGetCashParams;
 import ru.otus.hw15.messageSystem.Address;
@@ -9,19 +11,23 @@ import ru.otus.hw15.messageSystem.Message;
 import ru.otus.hw15.messageSystem.MessageSystem;
 import ru.otus.hw15.messageSystem.MessageSystemContext;
 import ru.otus.hw15.model.CacheParams;
-import ru.otus.hw15.ws.WebSocket;
 
 import javax.annotation.PostConstruct;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @Component
+@ServerEndpoint(value = "/cache_web_socket", configurator = SpringConfigurator.class)
+@Controller
 public class FrontendServiceImpl implements FrontendService {
     private final static Logger logger = Logger.getLogger(FrontendServiceImpl.class.getName());
 
     @Autowired
     private Address frontAddress;
-    @Autowired
-    private WebSocket socket;
+
+    private Session session;
 
     private final MessageSystemContext context;
 
@@ -55,6 +61,40 @@ public class FrontendServiceImpl implements FrontendService {
     @Override
     public void sendCache(CacheParams cacheParams) {
         logger.info("in send cache report, cacheParams: " + cacheParams);
-        socket.sendMessage(cacheParams.toString());
+        sendMessage(cacheParams.toString());
+    }
+
+    @OnOpen
+    public void open(Session session) {
+        logger.info("open");
+    }
+
+    @OnClose
+    public void closedConnection(Session session) {
+        logger.info("close");
+    }
+
+    @OnError
+    public void error(Session session, Throwable t) {
+        logger.info("open");
+    }
+
+
+    @OnMessage
+    public void onMessage(Session session, String msg) {
+        logger.info("in webSocket, sessionId = " + session.getId());
+        this.session = session;
+        getCacheReport();
+    }
+
+    public void sendMessage(String cacheParams) {
+        try {
+            for (Session sess : session.getOpenSessions()) {
+                if (sess.isOpen())
+                    sess.getBasicRemote().sendText(cacheParams);
+            }
+        } catch (IOException e) {
+            logger.info(e.getMessage());
+        }
     }
 }
